de50f7fbf5a734a434f142e26960f75677577a8c
Unknown <michellopez_16@hotmail.com>
2018-02-08 08:02:24 -0600

    Modulo 2 y 3 basico
    Falta añadir bien el modulo 2

---
+++ Modulo3Simat/7_PSO/SwarmEjem.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Nov 27 10:12:26 2017
+
+@author: MichelMacSDD
+"""
+
+import SwarmPackagePy
+from SwarmPackagePy import testFunctions as tf
+from SwarmPackagePy import animation, animation3D
+
+
+alh = SwarmPackagePy.pso(50, tf.easom_function, -10, 10, 2, 20,w=0.5, c1=1, c2=1)
+                         
+animation(alh.get_agents(), tf.easom_function, -10, 10)
+animation3D(alh.get_agents(), tf.easom_function, -10, 10)
+
+
